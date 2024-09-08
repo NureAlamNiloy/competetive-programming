51755cfe75aadb6857bdfa07586e73b479c7780b
Niloy74779 <mdniloy74779@gmail.com>
2024-09-08 19:25:45 +0600

    new problems

--- problem.cpp
+++ problem.cpp
@@ -12,30 +12,22 @@ int main()
     fraction();
     optimize();
 
-    int i,n,sum=0,x=0,coin=0;
-    cin >> n;
-    int arr[n];
-    for(i=0;i<n;i++){
-       cin >> arr[i]; 
-       sum+=arr[i];
-    } 
-    for(i=0;i<n;i++){
-        for(int j=0; j<n; j++){
-            if(arr[i]>arr[j]){
-                int tamp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = tamp;
-            }
+    int arr[8];
+    for(int i=0; i<8; i++){
+        cin >> arr[i];
+    }
+    for(int i=0; i<8; i++){
+        if(arr[i]<100 || arr[i]>675 || arr[i]%25 !=0){
+            cout << "No" << endl;
+            return 0;
+        }
+        if(i==0) continue;
+        if(arr[i]< arr[i-1]){
+            cout << "No" << endl;
+            return 0;
         }
     }
-    for(i=0;i<n; i++){
-        x += arr[i];
-        coin++;
-        if(x>(sum-x)) break;
-
-    }
-    cout << coin << endl;
-    
+    cout << "Yes" << endl;
 	return 0;
 }
 
