2c61a9ee6e87af4a73b6b85a023a1b482a79acfa
NureAlamNiloy <mdniloy74779@gmail.com>
2025-10-12 11:09:14 +0600

    niloy

---
+++ Python/Lacture1.py
@@ -0,0 +1,14 @@
+n = int(input())
+for i in range(1,n+1):
+    if i%3==0 and i%5==0:
+        print('"FizzBuzz"', end=",")
+    elif i%3 == 0:
+        print('"Fizz"', end=",")
+    elif i%5 ==0:
+        print('"Buzz"', end=",")
+    else:
+        print(f'"{i}"', end=",")
+    
+    if i == n:
+        print(end="")
+
